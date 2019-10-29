8afcf66d6f377e81b972c85e2ce4408e9d560641
feosilverada <57150112+feosilverada@users.noreply.github.com>
2019-10-29 20:42:03 +0300

    Update test_for_school.js

--- test_for_school.js
+++ test_for_school.js
@@ -1,2 +1,13 @@
-function compare(){
-}
+    function compare( a, b ){
+        
+        if( a < b ) return 1;
+        if( a == b ) return 0;
+        if( a > b ) return -1;
+        
+    }
+    
+    var data  = [100, 14, 57, 90, 4378, 99, 345];
+    
+    data.sort( compare );
+    
+    console.log( data[0] );
